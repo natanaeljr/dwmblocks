e1318f1030301f70a63c95b0727b21883fe5afbc
Natanael Rabello <natanael.rabello@outlook.com>
2020-09-12 20:34:00 -0300

    New date format
    Signed-off-by: Natanael Rabello <natanael.rabello@outlook.com>

--- blocks.h
+++ blocks.h
@@ -1,11 +1,11 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Notes: ", "cat ~/.notes-oneline",	                  60,		0},
-	{"Bat: ", "acpi | awk '{print $4 $5}' | tr ',' ' '",	30,		0},
+	{"", "cat ~/.notes-oneline",	                        60,		0},
+	{"BAT: ", "acpi | awk '{print $4 $5}' | tr ',' ' '",	30,		0},
 	{"CPU: ", "awk -f ~/.local/bin/cpuawk",	              2,		0},
-	{"Mem: ", "free -h | awk '/Mem/ { print $3 }'",	      5,		0},
-	{"", "date",					                                1,		0},
+	{"MEM: ", "free -h | awk '/Mem/ { print $3 }'",	      5,		0},
+	{"", "date +'%a %b %d %Y %T %p'",			                1,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
