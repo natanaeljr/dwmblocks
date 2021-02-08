f95a8f714bedce9d8a97b20a4334213b9abdeeaf
Natanael Rabello <natanael.rabello@outlook.com>
2021-02-08 16:33:52 -0300

    Add Bitcoin block
    Signed-off-by: Natanael Rabello <natanael.rabello@outlook.com>

--- blocks.h
+++ blocks.h
@@ -2,6 +2,7 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"", "cat ~/.notes-oneline",	                        60,		0},
+	{"BTC: ", R"(bitcoin_brl | LC_NUMERIC=pt_BR.UTF-8 xargs printf "R$%'.2f \n")",	30,		0},
 	{"CPU: ", "awk -f ~/.local/bin/cpuawk",	              2,		0},
 	{"MEM: ", "free -h | awk '/Mem/ { print $3 }'",	      5,		0},
 	{"", "date +'%a %b %d %Y %T %p'",			                1,		0},
