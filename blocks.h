a31b6a959728527b6bba6a7c5a6715d8364fd251
Natanael Rabello <natanael.rabello@outlook.com>
2021-02-10 08:04:08 -0300

    Compute actual used Memory
    Signed-off-by: Natanael Rabello <natanael.rabello@outlook.com>

--- blocks.h
+++ blocks.h
@@ -4,7 +4,7 @@ static const Block blocks[] = {
 	{"", "cat ~/.notes-oneline",	                        60,		0},
 	{"BTC: ", R"(bitcoin_brl | LC_NUMERIC=pt_BR.UTF-8 xargs printf "R$%'.2f \n")",	30,		0},
 	{"CPU: ", "cpu_percent_temp",	              2,		0},
-	{"MEM: ", "free -h | awk '/Mem/ { print $3 }'",	      5,		0},
+	{"MEM: ", R"(free -m | awk '/Mem:/ {used=$2-$7; unit="MiB"; if(used>=1000) {used/=1024; unit="GiB"}; printf("%.1f%s\n",used,unit)}')",	      3,		0},
 	{"", "date +'%a %b %d %Y %T %p'",			                1,		0},
 };
 
