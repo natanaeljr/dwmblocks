aab8fdc9129ed8cc837af0a80c04fdab51a0034a
Natanael Rabello <natanael.rabello@outlook.com>
2021-02-14 19:56:01 -0300

    CPU using sensors-cached for % and °C
    Signed-off-by: Natanael Rabello <natanael.rabello@outlook.com>

--- blocks.h
+++ blocks.h
@@ -1,12 +1,12 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "cat ~/.notes-oneline",	                        60,		0},
+	{"", R"(cat ~/.notes-oneline)",	                        60,		0},
 	{"BTC: ", R"(bitcoin_brl | LC_NUMERIC=pt_BR.UTF-8 xargs printf "R$%'.2f \n")",	30,		0},
 	{"GPU: ", R"(nvidia-smi --query-gpu="utilization.gpu,temperature.gpu,memory.used,fan.speed" --format=csv,noheader |tr -d ',' | awk '{printf "[%02d%% %02d°C %d%s %02d%%]\n", $1, $3, $4, $5, $6}')",	2,		0},
-	{"CPU: ", "cpu_percent_temp",	              2,		0},
+	{"CPU: ", R"((awk -f ~/.local/bin/cpuawk; sensors-cached | awk '/CPU:/ {printf $2}' | tr -d '+°C') | xargs | awk '{printf "[%s %02.0f°C]\n", $1, $2}')",	              2,		0},
 	{"MEM: ", R"(free -m | awk '/Mem:/ {used=$2-$7; unit="MiB"; if(used>=1000) {used/=1024; unit="GiB"}; printf("%.1f%s\n",used,unit)}')",	      3,		0},
-	{"", "date +'%a %b %d %Y %T %p'",			                1,		0},
+	{"", R"(date +'%a %b %d %Y %T %p')",			                1,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
