267544ee1c80a050f539883b41b985bcf93cf8b3
Natanael Rabello <natanael.rabello@outlook.com>
2021-02-15 10:19:13 -0300

    Memory block: use float format to round percentage
    Signed-off-by: Natanael Rabello <natanael.rabello@outlook.com>

--- blocks.h
+++ blocks.h
@@ -6,7 +6,7 @@ static const Block blocks[] = {
 {"CPU:", R"((cpu-percent; sensors-cached | awk '/CPU_FAN/{print $2} /CPU_TEMP/{print $2}' | tr -d '+°C') | xargs | awk '{printf "[%02d%% %02.0f°C %drmp]\n",$1,$3,$2}')", 2, 0},
 {"FAN:", R"(sensors-cached | awk '/SYS_FAN/ {print $2}' | xargs printf "[%d %drpm]\n")", 3, 0},
 {"TEMP:",R"(sensors-cached | awk '/_TEMP/ {print $2}' | tr -d '+°C' | xargs | awk '{printf "[%d %d %d %d°C]\n",$1,$2,$5,$6}')", 3, 0},
-{"MEM:", R"(free -m | awk '/Mem:/ {used=$2-$7; perc=used*100/$2; unit="MiB"; if(used>=1000) {used/=1024; unit="GiB"}; printf("[%02d%% %.1f%s]\n",perc,used,unit)}')", 3, 0},
+{"MEM:", R"(free -m | awk '/Mem:/ {used=$2-$7; perc=used*100/$2; unit="MiB"; if(used>=1000) {used/=1024; unit="GiB"}; printf("[%02.f%% %.1f%s]\n",perc,used,unit)}')", 3, 0},
 {"", R"(date +'(%a) %b %d %Y %T %p')", 1, 0},
 };
 
