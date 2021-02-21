d7fb46fc77af06852be814764f3917753e2a8c8d
Natanael Rabello <natanael.rabello@outlook.com>
2021-02-21 17:35:09 -0300

    Replace PLAY cmd for spotify-bar script
    Signed-off-by: Natanael Rabello <natanael.rabello@outlook.com>

--- blocks.h
+++ blocks.h
@@ -1,22 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 /*Icon*/  /*Command*/  /*Update Interval*/  /*Update Signal*/
-{"PLAY:",R"(
-  case $BUTTON in
-    1) timeout 8 playerctl --player=spotifyd previous & ;;
-    2) timeout 8 playerctl --player=spotifyd play-pause & ;;
-    3) timeout 8 playerctl --player=spotifyd next & ;;
-  esac
-  # TODO spawn cmd on the fly to get the playing title
-  # - save it to a cache file
-  # - launch it on background and send a signal to update the block when finished
-  # - compute the duration to end the music and launch a timer on background to update
-  #   when the music is ending, so the title is synced
-  # - launch bg process each time a button is clicked or it ticks
-  # ( timeout 10 playerctl --player=spotifyd metadata --format "{{ title }}" )
-  echo [spotify]
-)", 60, 8},
-
+{"PLAY:",R"(SIGNAL=8 spotify-bar)", 60, 8},
 {"BTC:", R"(curl -s https://www.mercadobitcoin.net/api/BTC/ticker/ | jq '.ticker.last' | LC_NUMERIC=pt_BR.UTF-8 xargs printf "[R$%'.2f]\n")", 60, 7},
 {"GPU:", R"(nvidia-smi --query-gpu="utilization.gpu,temperature.gpu,memory.used,fan.speed" --format=csv,noheader | tr -d ',' | awk '{printf "[%02d%% %02d°C %d%s %02d%%]\n",$1,$3,$4,$5,$6}')", 2, 6},
 {"CPU:", R"((cpu-percent; sensors-cached | awk '/CPU_FAN/{print $2} /CPU_TEMP/{print $2}' | tr -d '+°C') | xargs | awk '{printf "[%02d%% %02.0f°C %drmp]\n",$1,$3,$2}')", 2, 5},
